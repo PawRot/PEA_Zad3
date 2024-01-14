d303165741d1e0396f8ba209ec9d7fd25b3756fc
Michał Zychowicz <zychomichal@gmail.com>
2024-01-14 17:17:02 +0100

    Work on genetic algorithm

--- algorithms/geneticBase.cpp
+++ algorithms/geneticBase.cpp
@@ -3,3 +3,109 @@
 //
 
 #include "geneticBase.h"
+
+
+genetic::genetic(const std::vector<std::vector<int>>& matrix, const int stopCriterion, const int populationSize,
+                 const long double crossoverProbability, const long double mutationProbability) {
+    this->matrix = matrix;
+    this->stopCriterion = stopCriterion;
+    this->populationSize = populationSize;
+    this->crossoverProbability = crossoverProbability;
+    this->mutationProbability = mutationProbability;
+    numberOfCities = static_cast<int>(matrix.size());
+
+    for (int i = 0; i < populationSize; i++) {
+        std::vector<int> path;
+        path.reserve(numberOfCities);
+        for (int j = 0; j < numberOfCities; j++) {
+            path.push_back(j);
+        }
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::shuffle(path.begin(), path.end(), mt);
+        population.push_back(path);
+    }
+
+    auto populationCosts = std::vector<int>(populationSize);
+    for (const auto& path : population) { // calculate the costs of the paths in the initial population
+        populationCosts.push_back(pathCost(path));
+    }
+
+    bestPath = population[std::ranges::min_element(populationCosts) - populationCosts.begin()]; // find the best path
+
+    bestSolution = {pathCost(bestPath), bestPath, std::chrono::duration<float>::zero()};
+
+}
+
+std::tuple<int, std::vector<int>, std::chrono::duration<float>> genetic::geneticAlgorithm() {
+
+    std::tuple<int, std::vector<int>, std::chrono::duration<float>> bestSolution = {INT_MAX, {}, std::chrono::duration<float>::zero()};
+
+    std::chrono::duration<float> timeElapsed = std::chrono::duration<float>::zero();
+    const auto start = std::chrono::steady_clock::now(); // start time
+
+    while (std::chrono::duration_cast<std::chrono::seconds>(timeElapsed) <= std::chrono::seconds(stopCriterion)) {
+
+        std::vector<std::vector<int>> newPopulation; // container for the new population
+
+        populationCosts.clear();
+        // Selection for crossover
+        for (const auto& path : population) { // calculate the costs of the paths in the population
+            populationCosts.push_back(pathCost(path));
+        }
+
+        // tournament selection
+        for (int i = 0; i < populationSize; i++) {
+            int currentWinnerCost = INT_MAX;
+            int currentWinnerIndex = -1;
+
+            for (int k = 0; k < tournamentSize; k++) {
+                std::random_device rd;
+                std::mt19937 mt(rd());
+                std::uniform_int_distribution index(0, populationSize - 1);
+                const int randomIndex = index(mt);
+                if (populationCosts[randomIndex] < currentWinnerCost) {
+                    currentWinnerCost = populationCosts[randomIndex];
+                    currentWinnerIndex = randomIndex;
+                }
+            }
+            newPopulation.push_back(population[currentWinnerIndex]);
+        }
+
+        // crossover
+        for (int i = 0; i < populationSize; i += 2) {
+            std::random_device rd;
+            std::mt19937 mt(rd());
+            std::uniform_real_distribution<double> dist(0.0, 1.0);
+            if (dist(mt) < crossoverProbability) { // if the crossover is to be performed
+                std::random_device rd;
+                std::mt19937 mt(rd());
+                std::uniform_int_distribution index(0, numberOfCities - 1);
+                const int randomIndex = index(mt);
+                std::vector<int> child1;
+                std::vector<int> child2;
+                child1.reserve(numberOfCities);
+                child2.reserve(numberOfCities);
+                crossing(i, i + 1, child1, child2, randomIndex); // perform the crossover
+                newPopulation[i] = child1;
+                newPopulation[i + 1] = child2;
+            }
+        }
+
+
+
+        timeElapsed = std::chrono::steady_clock::now() - start; // time elapsed from the start of the algorithm
+    }
+
+    return bestSolution;
+
+}
+
+int genetic::pathCost(const std::vector<int>&testedPath) const {
+    int cost = 0;
+    for (int i = 0; i < numberOfCities - 1; i++) {
+        cost += matrix[testedPath[i]][testedPath[i + 1]];
+    }
+    cost += matrix[testedPath[numberOfCities - 1]][testedPath[0]]; // Return to the starting city
+    return cost;
+}
--- algorithms/geneticBase.h
+++ algorithms/geneticBase.h
@@ -5,9 +5,54 @@
 #ifndef GENETIC_H
 #define GENETIC_H
 
+#include <vector>
+#include <chrono>
+#include <random>
+#include <algorithm>
+
 
 
 class genetic {
+    std::vector<std::vector<int>> matrix; // data matrix
+    std::vector<int> bestPath; // current best path
+    int numberOfCities; // number of cities
+    int populationSize; // size of the population
+    int stopCriterion; // stop criterion in seconds
+    int tournamentSize = 5; // size of the tournament
+    long double crossoverProbability; // probability of crossover
+    long double mutationProbability; // probability of mutation
+    int numberOfGenerations = 0; // number of generations
+
+    std::vector<std::vector<int>> population; // current population
+    std::vector<int> populationCosts; // costs of the paths in the population
+
+    std::tuple<int, std::vector<int>, std::chrono::duration<float>> bestSolution
+
+    std::vector<std::string> betterPathsAndTimes; // vector of better paths and times when they were found
+
+    [[nodiscard]] int pathCost(const std::vector<int>& testedPath) const; // calculates the curent path cost
+
+protected:
+    genetic(const std::vector<std::vector<int>> &matrix, int stopCriterion, int populationSize, long double crossoverProbability, long double mutationProbability);
+    ~genetic() = default;
+
+    virtual void crossing(int parent1, int parent2, int child1, int child2) = 0; // virtual method for crossing, will be implemented in the derived classes
+
+
+
+public:
+
+
+    // int - cost, vector - path, duration - time of the algorithm
+    std::tuple<int, std::vector<int>, std::chrono::duration<float>> geneticAlgorithm(); // runs the algorithm
+
+    [[nodiscard]] auto getBetterPathsAndTimes() const {
+        return betterPathsAndTimes;
+    }
+
+    [[nodiscard]] int getNumberOfGenerations() const {
+        return numberOfGenerations;
+    }
 
 };
 
