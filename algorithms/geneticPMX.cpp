3545e2218bee1e890d87fc4a883ea73939e555a5
Michał Zychowicz <zychomichal@gmail.com>
2024-01-15 02:18:44 +0100

    Very slow PMX implementation

--- algorithms/geneticPMX.cpp
+++ algorithms/geneticPMX.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "geneticPMX.h"
-#include <iostream>
 
 geneticPMX::geneticPMX(const std::vector<std::vector<int>>&matrix, const int stopCriterion, const int populationSize,
                        const long double crossoverProbability, const long double mutationProbability) : genetic(
@@ -13,19 +12,6 @@ geneticPMX::geneticPMX(const std::vector<std::vector<int>>&matrix, const int sto
     mutationProbability) {
 }
 
-// std::pair<std::vector<int>, std::vector<int>> geneticPMX::crossing(std::vector<int> parent1, std::vector<int> parent2) {
-//     // select random range
-//     std::random_device rd;
-//     std::mt19937 mt(rd());
-//     std::uniform_int_distribution<int> dist(0, numberOfCities - 1);
-//     int rangeStart = dist(mt);
-//     int rangeEnd = dist(mt);
-//     if (rangeStart > rangeEnd) {
-//         std::swap(rangeStart, rangeEnd);
-//     }
-//
-//
-// }
 
 std::pair<std::vector<int>, std::vector<int>> geneticPMX::crossing(std::vector<int> parent1, std::vector<int> parent2) {
     // select random range
@@ -50,53 +36,54 @@ std::pair<std::vector<int>, std::vector<int>> geneticPMX::crossing(std::vector<i
     // copy the selected range from parent2 to offspring2
     std::copy(parent2.begin() + rangeStart, parent2.begin() + rangeEnd + 1, offspring2.begin() + rangeStart);
 
-    // create a map of the genes in the selected range
-    // auto map1 = std::unordered_map<int, int>();
-    // auto map2 = std::unordered_map<int, int>();
-    // for (int i = rangeStart; i <= rangeEnd; i++) {
-    //     map1[parent1[i]] = parent2[i];
-    //     map2[parent2[i]] = parent1[i];
-    // }
-    std::cout << std::endl;
-
-    // print key and value of the maps
-    // for (const auto& [key, value] : map1) {
-    // std::cout << key << " " << value << std::endl;
-    // }
 
     // Insert genes that are in one parent's range but not in the other
     for (int i = rangeStart; i <= rangeEnd; i++) {
-        if (std::find(offspring1.begin(), offspring1.end(), parent2[i]) == offspring1.end()) {
+        if (std::find(offspring1.begin() + rangeStart, offspring1.begin() + rangeEnd + 1, parent2[i]) == offspring1.begin() + rangeEnd + 1) {
             const auto index = static_cast<int>(std::distance(parent2.begin(), std::find(parent2.begin(), parent2.end(), parent1[i])));
             if (offspring1[index] == -1) {
                 offspring1[index] = parent2[i];
             }
             else {
-                const auto index2 = static_cast<int>(std::distance(parent2.begin(),std::find(parent2.begin(), parent2.end(), parent1[index])));
+                auto index2 = static_cast<int>(std::distance(parent2.begin(),std::find(parent2.begin(), parent2.end(), parent1[index])));
+                while (offspring1[index2] != -1) {
+                     index2 = static_cast<int>(std::distance(parent2.begin(),std::find(parent2.begin(), parent2.end(), parent1[index2])));
+                }
                 offspring1[index2] = parent2[i];
+            }
+        }
+
+        if (std::find(offspring2.begin() + rangeStart, offspring2.begin() + rangeEnd + 1, parent1[i]) == offspring2.begin() + rangeEnd + 1) {
+            const auto index = static_cast<int>(std::distance(parent1.begin(), std::find(parent1.begin(), parent1.end(), parent2[i])));
+            if (offspring2[index] == -1) {
+                offspring2[index] = parent1[i];
+            }
+            else {
+                auto index2 = static_cast<int>(std::distance(parent1.begin(),std::find(parent1.begin(), parent1.end(), parent2[index])));
+                while (offspring1[index2] != -1) {
+                    index2 = static_cast<int>(std::distance(parent1.begin(),std::find(parent1.begin(), parent1.end(), parent2[index2])));
+                }
+                offspring2[index2] = parent1[i];
             }
         }
     }
 
-    // // Fill the remaining genes without duplicating any in the selected range
-    // for (int i = 0; i < numberOfCities; i++) {
-    //     if (i < rangeStart || i > rangeEnd) {
-    //         // If the offspring doesn't have the gene add it from the other parent
-    //         if (std::find(offspring1.begin(), offspring1.end(), parent2[i]) == offspring1.end()) {
-    //             offspring1[i] = parent2[i];
-    //         }
-    //         if (std::find(offspring2.begin(), offspring2.end(), parent1[i]) == offspring2.end()) {
-    //             offspring2[i] = parent1[i];
-    //         }
-    //     }
-    // }
-
-
-    // Fill the remaining genes without duplicating any in the selected range
-
-    // Rotate the offspring to match the parent's starting point
-    // std::rotate(offspring1.begin(), std::find(offspring1.begin(), offspring1.end(), parent1[0]), offspring1.end());
-    // std::rotate(offspring2.begin(), std::find(offspring2.begin(), offspring2.end(), parent2[0]), offspring2.end());
-
+    // Copy the remaining genes from parent2 to offspring1 and parent1 to offspring2
+    for (int i = 0; i < rangeStart; i++) {
+        if (offspring1[i] == -1) {
+            offspring1[i] = parent2[i];
+        }
+        if (offspring2[i] == -1) {
+            offspring2[i] = parent1[i];
+        }
+    }
+    for (int i = rangeEnd + 1; i < numberOfCities; i++) {
+        if (offspring1[i] == -1) {
+            offspring1[i] = parent2[i];
+        }
+        if (offspring2[i] == -1) {
+            offspring2[i] = parent1[i];
+        }
+    }
     return {offspring1, offspring2};
 }
